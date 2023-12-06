cf56c56057c0a9e4e814727e09bea293b967d5cc
Abdel Yahaya <abdelymaster@gmail.com>
2023-12-06 12:46:26 -0500

    Finished the first GUI draft

--- build-QT-Project-Folder-Desktop-Debug/ui_mainwindow.h
+++ build-QT-Project-Folder-Desktop-Debug/ui_mainwindow.h
@@ -19,6 +19,7 @@
 #include <QtWidgets/QSpinBox>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QTextBrowser>
+#include <QtWidgets/QTextEdit>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -39,6 +40,18 @@ public:
     QPlainTextEdit *plainTextEdit_6;
     QLabel *mainImage;
     QTextBrowser *centerDisplay;
+    QTextBrowser *checkPoint1;
+    QTextBrowser *checkPoint6;
+    QTextBrowser *checkPoint4;
+    QTextBrowser *checkPoint3;
+    QTextBrowser *checkPoint2;
+    QTextBrowser *checkPoint5;
+    QPushButton *shockButton;
+    QPushButton *onButton;
+    QTextBrowser *onSignal;
+    QPlainTextEdit *shockText;
+    QPlainTextEdit *timeText;
+    QTextEdit *instructionText;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -113,6 +126,62 @@ public:
         centerDisplay->setObjectName(QString::fromUtf8("centerDisplay"));
         centerDisplay->setGeometry(QRect(400, 180, 161, 81));
         centerDisplay->setStyleSheet(QString::fromUtf8("background-color:white;"));
+        checkPoint1 = new QTextBrowser(centralwidget);
+        checkPoint1->setObjectName(QString::fromUtf8("checkPoint1"));
+        checkPoint1->setGeometry(QRect(370, 240, 16, 16));
+        checkPoint1->setStyleSheet(QString::fromUtf8("height: 20px;\n"
+"width:20px;\n"
+"border-radius: 50%;\n"
+"background-color: grey;"));
+        checkPoint6 = new QTextBrowser(centralwidget);
+        checkPoint6->setObjectName(QString::fromUtf8("checkPoint6"));
+        checkPoint6->setGeometry(QRect(380, 370, 16, 16));
+        checkPoint6->setStyleSheet(QString::fromUtf8("border-radius: 50%;\n"
+"background-color: grey;"));
+        checkPoint4 = new QTextBrowser(centralwidget);
+        checkPoint4->setObjectName(QString::fromUtf8("checkPoint4"));
+        checkPoint4->setGeometry(QRect(560, 360, 16, 16));
+        checkPoint4->setStyleSheet(QString::fromUtf8("border-radius: 50%;\n"
+"background-color: grey;"));
+        checkPoint3 = new QTextBrowser(centralwidget);
+        checkPoint3->setObjectName(QString::fromUtf8("checkPoint3"));
+        checkPoint3->setGeometry(QRect(570, 240, 16, 16));
+        checkPoint3->setStyleSheet(QString::fromUtf8("border-radius: 50%;\n"
+"background-color: grey;"));
+        checkPoint2 = new QTextBrowser(centralwidget);
+        checkPoint2->setObjectName(QString::fromUtf8("checkPoint2"));
+        checkPoint2->setGeometry(QRect(500, 140, 16, 16));
+        checkPoint2->setStyleSheet(QString::fromUtf8("border-radius: 50%;\n"
+"background-color: grey;"));
+        checkPoint5 = new QTextBrowser(centralwidget);
+        checkPoint5->setObjectName(QString::fromUtf8("checkPoint5"));
+        checkPoint5->setGeometry(QRect(460, 290, 16, 16));
+        checkPoint5->setStyleSheet(QString::fromUtf8("border-radius: 50%;\n"
+"background-color: grey;"));
+        shockButton = new QPushButton(centralwidget);
+        shockButton->setObjectName(QString::fromUtf8("shockButton"));
+        shockButton->setGeometry(QRect(420, 330, 91, 25));
+        shockButton->setStyleSheet(QString::fromUtf8("background-color: white;"));
+        onButton = new QPushButton(centralwidget);
+        onButton->setObjectName(QString::fromUtf8("onButton"));
+        onButton->setGeometry(QRect(580, 450, 83, 25));
+        onSignal = new QTextBrowser(centralwidget);
+        onSignal->setObjectName(QString::fromUtf8("onSignal"));
+        onSignal->setGeometry(QRect(550, 450, 16, 16));
+        onSignal->setStyleSheet(QString::fromUtf8("border-radius: 50%;\n"
+"background-color: grey;"));
+        shockText = new QPlainTextEdit(centralwidget);
+        shockText->setObjectName(QString::fromUtf8("shockText"));
+        shockText->setGeometry(QRect(400, 180, 91, 31));
+        shockText->setStyleSheet(QString::fromUtf8("border: white;"));
+        timeText = new QPlainTextEdit(centralwidget);
+        timeText->setObjectName(QString::fromUtf8("timeText"));
+        timeText->setGeometry(QRect(510, 180, 51, 31));
+        timeText->setStyleSheet(QString::fromUtf8("border: white;"));
+        instructionText = new QTextEdit(centralwidget);
+        instructionText->setObjectName(QString::fromUtf8("instructionText"));
+        instructionText->setGeometry(QRect(403, 200, 151, 51));
+        instructionText->setStyleSheet(QString::fromUtf8("border: white;"));
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
@@ -143,6 +212,15 @@ public:
         plainTextEdit_5->setPlainText(QCoreApplication::translate("MainWindow", "5. Perform CPR", nullptr));
         plainTextEdit_6->setPlainText(QCoreApplication::translate("MainWindow", "6. Monitor Patient", nullptr));
         mainImage->setText(QString());
+        shockButton->setText(QCoreApplication::translate("MainWindow", "Apply Shock", nullptr));
+        onButton->setText(QCoreApplication::translate("MainWindow", "Turn On", nullptr));
+        shockText->setPlainText(QCoreApplication::translate("MainWindow", "SHOCKS 00", nullptr));
+        timeText->setPlainText(QCoreApplication::translate("MainWindow", "00:11", nullptr));
+        instructionText->setHtml(QCoreApplication::translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:'Sans'; font-size:10pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Follow Instructions</p></body></html>", nullptr));
     } // retranslateUi
 
 };

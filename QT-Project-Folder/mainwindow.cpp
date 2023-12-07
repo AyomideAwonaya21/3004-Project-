ef013cbaa79e06524ef5f3f03751aac48a04c83b
Ayomide Awonaya <78707812+AyomideAwonaya21@users.noreply.github.com>
2023-12-06 19:08:52 -0500

    Update mainwindow.cpp

--- QT-Project-Folder/mainwindow.cpp
+++ QT-Project-Folder/mainwindow.cpp
@@ -1,44 +1,61 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QPixmap>
-#include <QDir>
-#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
+    : QMainWindow(parent), ui(new Ui::MainWindow), aedSimulation() {
     ui->setupUi(this);
-    QString imagePath = QDir::currentPath() + "/Images/main.png";
-    qDebug() << imagePath;
-    QPixmap pix(imagePath);
+
+    // Set the main image using the provided path
+    QPixmap pix("/home/student/Desktop/Final Project/3004-Project-/QT-Project-Folder/Images/main.png");
     ui->mainImage->setPixmap(pix);
 
-    //the checkpoints are for the images, light them green when the image is active and play audio
-    //ui->checkPoint1;
-    //ui->checkPoint2;
-    //ui->checkPoint3;
-    //ui->checkPoint4;
-    //ui->checkPoint5;
-    //ui->checkPoint6;
+    // Connect buttons to their respective slots
+    connect(ui->shockButton, &QPushButton::clicked, this, &MainWindow::on_shockButton_clicked);
+    connect(ui->onButton, &QPushButton::clicked, this, &MainWindow::on_onButton_clicked);
 
-    //this is the button to apply the shock
-    //ui->shockButton;
+    // Start the AED simulation
+    aedSimulation.startSimulation();
 
-    //the components for turning AED on, flash signal to green when on
-    //ui->onSignal;
-    //ui->onButton;
-
-    //these texts are for the main display on the AED
-   // ui->shockText;
-    //ui->timeText;
-    //ui->instructionText;
-
-    resize(800,600);
+    // Set the initial size of the MainWindow
+    resize(800, 600);
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
+    // Stop the simulation before the MainWindow is destroyed
+    aedSimulation.stopSimulation();
     delete ui;
 }
 
+void MainWindow::on_shockButton_clicked() {
+    // Handle logic when the shock button is clicked
+    aedSimulation.deliverShock(); // Assuming this method exists in AEDSimulation
+    updateInterface();
+}
+
+void MainWindow::on_onButton_clicked() {
+    // Handle logic when the on button is clicked
+    aedSimulation.powerOn(); // Assuming this method exists in AEDSimulation
+    updateInterface();
+}
+
+void MainWindow::updateInterface() {
+    // Update the user interface elements based on the state of the AED simulation
+    ui->instructionText->setPlainText(QString::fromStdString(aedSimulation.getCurrentInstruction()));
+    ui->timeText->setPlainText(QString::fromStdString(aedSimulation.getCurrentTime()));
+    ui->shockText->setPlainText(QString("SHOCKS ") + QString::number(aedSimulation.getShockCount()));
+    ui->onSignal->setStyleSheet(aedSimulation.isPoweredOn() ? "background-color: green;" : "background-color: grey;");
+
+    // Update the checkpoint indicators based on the current step of the simulation
+    updateCheckpoints(aedSimulation.getCurrentStep());
+}
+
+void MainWindow::updateCheckpoints(int step) {
+    // Update the color of each checkpoint based on the current step of the simulation
+    ui->checkPoint1->setStyleSheet(step >= 1 ? "background-color: green;" : "background-color: grey;");
+    ui->checkPoint2->setStyleSheet(step >= 2 ? "background-color: green;" : "background-color: grey;");
+    ui->checkPoint3->setStyleSheet(step >= 3 ? "background-color: green;" : "background-color: grey;");
+    ui->checkPoint4->setStyleSheet(step >= 4 ? "background-color: green;" : "background-color: grey;");
+    ui->checkPoint5->setStyleSheet(step >= 5 ? "background-color: green;" : "background-color: grey;");
+    ui->checkPoint6->setStyleSheet(step >= 6 ? "background-color: green;" : "background-color: grey;");
+}

817bcec9ca38c16e43798e71f1710368134917a5
Ayomide Awonaya <78707812+AyomideAwonaya21@users.noreply.github.com>
2023-12-07 15:23:11 -0500

    Update mainwindow.cpp

--- QT-Project-Folder/mainwindow.cpp
+++ QT-Project-Folder/mainwindow.cpp
@@ -1,21 +1,28 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QPixmap>
+#include <QDir>
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent), ui(new Ui::MainWindow), aedSimulation() {
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
+
     ui->setupUi(this);
+    aedSimulation = new AEDSimulation(this); // Initialize AEDSimulation
 
     // Set the main image using the provided path
-    QPixmap pix("/home/student/Desktop/Final Project/3004-Project-/QT-Project-Folder/Images/main.png");
+    QString imagePath = QDir::currentPath() + "/Images/main.png";
+    QPixmap pix(imagePath);
     ui->mainImage->setPixmap(pix);
 
     // Connect buttons to their respective slots
     connect(ui->shockButton, &QPushButton::clicked, this, &MainWindow::on_shockButton_clicked);
     connect(ui->onButton, &QPushButton::clicked, this, &MainWindow::on_onButton_clicked);
 
+    // Connect the AEDSimulation signals to MainWindow slots
+    connect(aedSimulation, &AEDSimulation::updateInterfaceSignal, this, &MainWindow::updateInterface);
+
     // Start the AED simulation
-    aedSimulation.startSimulation();
+    aedSimulation->startSimulation();
 
     // Set the initial size of the MainWindow
     resize(800, 600);
@@ -23,31 +30,32 @@ MainWindow::MainWindow(QWidget *parent)
 
 MainWindow::~MainWindow() {
     // Stop the simulation before the MainWindow is destroyed
-    aedSimulation.stopSimulation();
+    aedSimulation->stopSimulation();
+    delete aedSimulation; // Delete the aedSimulation object
     delete ui;
 }
 
 void MainWindow::on_shockButton_clicked() {
     // Handle logic when the shock button is clicked
-    aedSimulation.deliverShock(); // Assuming this method exists in AEDSimulation
+    aedSimulation->deliverShock();
     updateInterface();
 }
 
 void MainWindow::on_onButton_clicked() {
     // Handle logic when the on button is clicked
-    aedSimulation.powerOn(); // Assuming this method exists in AEDSimulation
+    aedSimulation->powerOn();
     updateInterface();
 }
 
 void MainWindow::updateInterface() {
     // Update the user interface elements based on the state of the AED simulation
-    ui->instructionText->setPlainText(QString::fromStdString(aedSimulation.getCurrentInstruction()));
-    ui->timeText->setPlainText(QString::fromStdString(aedSimulation.getCurrentTime()));
-    ui->shockText->setPlainText(QString("SHOCKS ") + QString::number(aedSimulation.getShockCount()));
-    ui->onSignal->setStyleSheet(aedSimulation.isPoweredOn() ? "background-color: green;" : "background-color: grey;");
+    ui->instructionText->setPlainText(QString::fromStdString(aedSimulation->getCurrentInstruction()));
+    ui->timeText->setPlainText(QString::fromStdString(aedSimulation->getCurrentTime()));
+    ui->shockText->setPlainText(QString("SHOCKS ") + QString::number(aedSimulation->getShockCount()));
+    ui->onSignal->setStyleSheet(aedSimulation->isPoweredOn() ? "background-color: green;" : "background-color: grey;");
 
     // Update the checkpoint indicators based on the current step of the simulation
-    updateCheckpoints(aedSimulation.getCurrentStep());
+    updateCheckpoints(aedSimulation->getCurrentStep());
 }
 
 void MainWindow::updateCheckpoints(int step) {
